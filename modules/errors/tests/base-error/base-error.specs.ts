400975f7f995fe1ef021eaba19b553495598957a
Simon Corcos <simoncorcos@hop-child.com>
2020-08-12 14:08:11 -0400

    Repository setup

--- modules/errors/tests/base-error/base-error.specs.ts
+++ modules/errors/tests/base-error/base-error.specs.ts
@@ -4,16 +4,17 @@ import {assert, expect} from "soulmate/test-utils";
 
 describe('unit: errors/BaseError', () => {
 
-    it(`should be an instance of Error`, assertInstanceOfErrorClass)
-    it(`should contain the stack information of its innerError`, assertContainsInnerErrorStack)
+    it(`should be an instance of Error`, assertInstanceOfErrorClass);
+    it(`should contain the stack information of its innerError`, assertContainsInnerInErrorStack);
+    it(`should identify child classes as instances of BaseError in a catch clause`, assertChildClassInstanceOfBaseError)
 });
 
 function assertInstanceOfErrorClass() {
 
-    assert(new BaseError() instanceof Error);
+    assertInstanceOfInCatch(BaseError, Error);
 }
 
-function assertContainsInnerErrorStack() {
+function assertContainsInnerInErrorStack() {
 
     try { throw new Error(); }
     catch (innerError) {
@@ -22,5 +23,22 @@ function assertContainsInnerErrorStack() {
 
         expect(baseError.stack).to.exist;
         assert(baseError.stack!.includes(innerError.stack));
+    }
+}
+
+class ChildError extends BaseError {}
+
+function assertChildClassInstanceOfBaseError() {
+
+    assertInstanceOfInCatch(ChildError, Error);
+    assertInstanceOfInCatch(ChildError, BaseError);
+    assertInstanceOfInCatch(ChildError, ChildError);
+}
+
+function assertInstanceOfInCatch(errorToThrow: new () => BaseError, expectedErrorClass: new () => Error) {
+    try { throw new errorToThrow(); }
+    catch (caughtError) {
+        assert(caughtError instanceof expectedErrorClass,
+            `${errorToThrow.name} is not an instance of ${expectedErrorClass.name} in a catch clause!`);
     }
 }
